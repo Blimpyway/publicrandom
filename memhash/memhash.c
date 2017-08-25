c9250764d686bc98bfdef56211c40706a1e1999f
blimpyway <blimpyway@gmail.com>
2017-08-25 13:24:53 +0300

    More statistical checks on randomness, "LOOP" capability..

--- memhash/memhash.c
+++ memhash/memhash.c
@@ -1,3 +1,12 @@
+/*
+ * memhash.c - a time delay function throttled by memory latency
+ *
+ * Its purpose is to compute a delayed key, that means it doesn't
+ * have to "protect" the key longer than the time it is required to run.
+ * 
+ * Having a huge chaotic state should make it safe for this purpose.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,34 +14,36 @@
 
 
 
-// here are kept the 256 bitinput key, shuffling state and output key
-unsigned int hpipe[] = {0, 0, 0, 0, 0, 0, 0, 0} ;
+// here are kept the 256 bit input key, scranbling state and output key
+unsigned int keybuf[] = {0, 0, 0, 0, 0, 0, 0, 0} ;
 
 
 
-// the biglist is a large array of random 32 bit ints, the impacts is a copy
-// of it used only for statistics (counting changes)
+// the bigbuf is a large array of random 32 bit ints, the impacts is a copy
+// of it used only for statistics (counting changes).
 // They are allocated in main() 
-unsigned int *biglist, *impacts;
+unsigned int *bigbuf, *impacts;
+
+// Updates "keybuf" - where input, scrambling and output states are kept
+void set_key(char *hexkey) {
+    sscanf(hexkey, "%08x%08x%08x%08x%08x%08x%08x%08x",
+        &keybuf[0], &keybuf[1], &keybuf[2], &keybuf[3], 
+        &keybuf[4], &keybuf[5], &keybuf[6], &keybuf[7]); 
+}
 
 // Different "working sizes" 49978001 104729 27644437
 // Not sure if prime number size has any impact on randomness
 // If small enough to fit in cache it will run much faster
-// Updates "hpipe" - where input, shuffling and output states are kept
-void set_key(char *bignum) {
-    sscanf(bignum, "%08x%08x%08x%08x%08x%08x%08x%08x",
-        &hpipe[0], &hpipe[1], &hpipe[2], &hpipe[3], 
-        &hpipe[4], &hpipe[5], &hpipe[6], &hpipe[7]); 
-}
-
-unsigned int numk = 49978001 ; 
+// Pulled off my sleeve a prime smaller than 64M ints which 
+// will occupy almost 256MByte memory space for each bigbuf and impacts
+unsigned int numk = 67108859 ; 
 
 void reset_impacts() {
-    memcpy(biglist, impacts, numk * sizeof(biglist[0]));
+    memcpy(bigbuf, impacts, numk * sizeof(bigbuf[0]));
 }
 
 // returns 0 on failing to load the big array of ints
-int load_biglist(char *fileName) {
+int load_bigbuf(char *fileName) {
     FILE *fp;
     fp = fopen(fileName, "r");
     if (fp == NULL) return 0;
@@ -42,58 +53,76 @@ int load_biglist(char *fileName) {
     return numb;
 }
 
-// Shows how much of the biglist was changed.
+// Shows how much of the bigbuf was changed.
 unsigned int numimpacts = 0;
 float zero_impacts() {
 	unsigned int i;
     numimpacts = 0;
 	for (i = 0; i < numk; i++) {
-        numimpacts += (impacts[i] != biglist[i]); 
+        numimpacts += (impacts[i] != bigbuf[i]); 
 	}
 	return (float) numimpacts / (float) numk;
 }
 
 void show_keys(char *label) {
     printf("%s %08x %08x %08x %08x %08x %08x %08x %08x\n", label, 
-            hpipe[0], hpipe[1], hpipe[2], hpipe[3], 
-            hpipe[4], hpipe[5], hpipe[6], hpipe[7]);
+            keybuf[0], keybuf[1], keybuf[2], keybuf[3], 
+            keybuf[4], keybuf[5], keybuf[6], keybuf[7]);
 }
 
 
 // Rotate left one bit. MSB becomes LSB
-#define rotate(x) ((x > 2147483647) + x << 1)
-
-// the shuffling function. Alters both hpipe and the big random array
-void shuffle(unsigned int numsteps) {
-    register  unsigned int pp, pos, t, i;
-    pp = 0; pos = numk - 1; t = 1; i = 0;
+// #define rotate(x) ((x > 0x7FFFFFFF) | (x << 1))
+#define rotate(x) ((x >> 31) | (x << 1))
+#define rotate5(x) ((x >> 27) | (x << 5))
+#define lrotate(x) ((x >> 61) | (x << 3))
+// the scrambling function. Reads&writes both keybuf and the big random array
+// with large enough bigbuf its speed is limited by memory latency
+void scramble(unsigned int numsteps) {
+    register  unsigned int k, pos, i, t;
+    k = 0; pos = numk - 1; t = 1; i = 0;
     while(i < numsteps) {
-        pp = i++ % 8;
-        t ^= biglist[pos] + hpipe[pp];
-        pos = t % numk;
-        biglist[pos] ^= rotate(t);
-        hpipe[pp] ^= t;
+        t ^= bigbuf[pos] + ~keybuf[k];
+        k = i++ % 8; 
+        pos = t % numk; 
+        bigbuf[pos] ^= ~t;
+        // keybuf[k] ^= t = rotate(t);
+        keybuf[k] ^= t = rotate5(t);
     }
 }
 
-// Just measures random lookups into biglist
+
+// hops aimlessly into bigbuf, to compare its speed with shuffling speed
+// A speed close to shuffling speed gives a hint of how expensive is 
+// actual shuffling processing vs. random memory updates
 // if the return value is not used the gcc optimiser may skip it entirely
 unsigned int  seekSpeed(unsigned int numsteps) {
-	register unsigned int i, pos; 
-    i = 0; pos = numsteps % numk;
-	while (i < numsteps) {
-		pos = (biglist[pos] + i++) % numk;
+	register unsigned int i = 0, pos; 
+    pos = numsteps % numk;
+	while (i++ < numsteps) {
+		pos = (bigbuf[pos] ^ i) % numk;
 	}
-    return biglist[pos];
+    return bigbuf[pos];
 }
 
-int LOOPS = 0;
+
+char *usage ="\n\
+\tUsage:\n\n \
+\t %s <num rounds> <key>\n\n\
+\t where <num rounds> is the number of shuffle rounds, \n\
+\t and <key> is a hex-encoded 256bit initial key\n\n";
 
 void main (int argc, char* argv[]) {
     unsigned int loopsize;
 
-    char *nloops = getenv("MEMHASHLOOPS");
-    if (nloops != NULL)  sscanf(nloops, "%d", &LOOPS);
+    if (argc < 3) {
+        printf(usage, argv[0]);
+        exit(1);
+    }
+
+    int LOOPS = 0;
+    char *sloops = getenv("MEMHASHLOOPS");
+    if (sloops != NULL)  sscanf(sloops, "%d", &LOOPS);
     
 
     const char *MEMHASHSIZE = (char *) getenv("MEMHASHSIZE");
@@ -104,9 +133,9 @@ void main (int argc, char* argv[]) {
             printf( "MEMHASHSIZE = %s\n", MEMHASHSIZE);
     } else 
         if (!LOOPS) 
-            printf ("MEMHASHSIZE undeclared in environment\n");
+            printf ("MEMHASHSIZE not in environment\n");
 
-    biglist = malloc(numk * sizeof(unsigned int));
+    bigbuf = malloc(numk * sizeof(unsigned int));
     impacts = malloc(numk * sizeof(unsigned int));
 
     sscanf(argv[1], "%d", &loopsize);
@@ -114,13 +143,13 @@ void main (int argc, char* argv[]) {
     set_key(argv[2]);
 
     if (!LOOPS)
-        printf("size of biglist is %d bytes, running %d cycles \n\n",  
+        printf("size of bigbuf is %d bytes, running %d cycles \n\n",  
                 numk * 4, loopsize
                );
 
     char *MEMHASHFILE = getenv("MEMHASHFILE");
-    if (MEMHASHFILE == NULL) MEMHASHFILE = "./random.bin";
-    if (!load_biglist(MEMHASHFILE)) {
+    if (MEMHASHFILE == NULL) MEMHASHFILE = "./bigbuf.dat";
+    if (!load_bigbuf(MEMHASHFILE)) {
         // exit(1);
         printf("MEMHASHFILE %s not found, using zero-filled big array\n",
                 MEMHASHFILE);
@@ -129,30 +158,33 @@ void main (int argc, char* argv[]) {
     if (LOOPS) {
         int i;
         for (i = 0; i < LOOPS; i++) {
-            shuffle(loopsize);
+            scramble(loopsize);
             printf("%08x%08x%08x%08x%08x%08x%08x%08x\n",
-            hpipe[0], hpipe[1], hpipe[2], hpipe[3], 
-            hpipe[4], hpipe[5], hpipe[6], hpipe[7]);
+            keybuf[0], keybuf[1], keybuf[2], keybuf[3], 
+            keybuf[4], keybuf[5], keybuf[6], keybuf[7]);
         }
     } else {
         show_keys("input :");
-
         clock_t stime = clock();
-        // shuffling is the "workhorse" KDF
-        shuffle(loopsize);
+        scramble(loopsize);
         stime = clock() - stime;
-        printf("Done in %4.5f sec, changed in biglist is %2.5f %%", 
-                ((float) stime / CLOCKS_PER_SEC), 
-                zero_impacts() * 100.0 );
-        printf(" (%d multi hits)\n", loopsize - numimpacts);
-
+        float tseconds = (float) stime / CLOCKS_PER_SEC ;
         show_keys("output:");
+        printf("\nDone in %4.4f sec,  %4.2f ns/step, bigbuf %2.5f%% changed,", 
+                tseconds,
+                (tseconds / loopsize * 1000000000.0), 
+                zero_impacts() * 100.0 );
+        printf(" %u multi hit\n", loopsize - numimpacts);
 
+        // Measure latency of random reads in bigbuf
+        if (loopsize > 2000000) loopsize = 2000000;
         stime = clock();
-        // Just do randomised reads from bignum array without writing
-        unsigned int ss = seekSpeed(loopsize);
+        unsigned int ss = seekSpeed(loopsize); 
+        // Use the result or optimizer skips it
         stime = clock() - stime;
-        printf("Random lookup time: %4.3f sec  -- %d\n",((float) stime / CLOCKS_PER_SEC), ss);
+        printf("bigbuf seek latency: %3.2f ns %u\b\b   \n",
+                ((float) stime / CLOCKS_PER_SEC / loopsize * 1000000000.0), 
+                ss / ss );
     }
     exit(0);
 }
--- memhash/memhash.sh
+++ memhash/memhash.sh
@@ -1,9 +1,12 @@
 #!/bin/sh 
 #
-# Utilty to build and run memhash.c large memory key generator. 
+# Utilty to compile and run memhash.c key generator. 
 # The argument is a hexadecimal 256 bit integer, which is an input for 
-# the key generation program
-# it outputs a 256bit hex encoded key generated from input after $ROUNDS rounds
+# the key generation program.
+# it outputs an encoded key, with the same format, generated from input after 
+# $ROUNDS rounds
+# 
+# The following parameters are passed to C binary via environment export
 # 
 # MEMHASHFILE is an initial state that is shuffled together with the input
 # to produce the hashed output. 
@@ -15,61 +18,64 @@
 #
 # MEMHASHLOOPS 
 # - if zero the program is run only once (one loop) in verbose mode
-# - is set to a number > 0 it repeats ROUNDS MEMHASHLOOPS times , 
+# - is set to a value > 0 it repeats ROUNDS MEMHASHLOOPS times , 
 # printing an intermediate output line after each repeating ROUNDS
-MEMHASHLOOPS=100
+MEMHASHLOOPS=0
 
-# On my I5 2.9GHz laptop the large memory latency is 88ns
+# On my I5 1.9GHz laptop the large memory latency is 88ns
 
 
 # Number of shuffle rounds. Each round updates an uint32 in both 
 # 256bit input/output array and the large shuffling memory array
-ROUNDS=2000000
+ROUNDS=100000000
 
 # Big random uint32 array size. Use smaller (less than cpu cache) values
 # to see how speed increases when this runs in cache
 # Note: this is the size in number of uint32 entries, the byte size quadruples
-MEMHASHSIZE=49978001
-MEMHASHSIZE=15485863
+# MEMHASHSIZE=49978001
+# MEMHASHSIZE=15485863
 
-# a 256 MB array, big enough for "desktop" hashes
-MEMHASHSIZE=67108864
-# MEMHASHSIZE=52428799
-# MEMHASHSIZE=20971519
+# ~256kbyte array if someone wants to compare regular speed vs cpu caches:
+# MEMHASHSIZE=65521
 
-# ~256kbyte array:
-# MEMHASHSIZE=65536
+# almost 256 MB array, big enough for "desktop" hashes
+MEMHASHSIZE=67108859
 
 #
 # A large file with random bits. By "random" it means they-re all the same
-# Use /dev/zero to test a uniform, non-random initial table
+
+# By default is a file called "bigbuf.dat" in the same directory with 
+# this script. A few lines down if not found, the bigbuf.dat is created 
+# by compiling and running mkbigbuf.c 
+MEMHASHFILE=bigbuf.dat
+
+# Use /dev/zero to test a zero-filled, non-chaotic initial bigbuf
+# outputs still look randomish.
 # MEMHASHFILE=/dev/zero
-
-# By default is a file called "random.bin" in the same directory with 
-# this script. A few lines down random.bin if not found then is created 
-# from /dev/urandom
-# This creation should be replaced with a standard predefined stream
-
-MEMHASHFILE=random.bin
 
 
 
 #
-# The default input for key derivation function
-HEXPASS=aaaaaaaabbbbbbbbccccccccddddddddeeeeeeeeffffffff0000000011111111
+# The default input key.
+# This just for tests, should be provided in command line
+HEXPASS=0000000011111111aaaaaaaabbbbbbbbccccccccddddddddeeeeeeeeffffffff
 [ -z "$1" ] || HEXPASS=$1
 
-# Creates a 256MB random.bin file, should well exceed most CPU caches
+# Creates a 256MB bigbuf.dat file, should well exceed most CPU caches
 cd `dirname $0`
-[ -f "./random.bin" ] || {
-    echo -n "Creating random.bin from /dev/urandom..."
-    dd if=/dev/urandom of=random.bin bs=4096 count=65536
-    echo "done!"
+[ -f "./bigbuf.dat" ] || {
+    echo -n "Creating chaotic bigbuf.dat ..."
+    [ -x ./mkbigbuf  ] || gcc -O3 -o mkbigbuf mkbigbuf.c
+    ./mkbigbuf bigbuf.dat    > genbigbuf.out
+    echo "done! - see few stats in genbigbuf.out some of their\n meaning in mkbigbuf.c\n"
 }
 
 
 export MEMHASHFILE MEMHASHSIZE MEMHASHLOOPS
 
-[ -x "memhash.bin" ] || gcc -o memhash.bin memhash.c
+[ -x "memhash" ] || {
+    echo "Compiling memhash.c .... "
+    gcc -o memhash memhash.c
+}
 
-./memhash.bin $ROUNDS $HEXPASS
+./memhash $ROUNDS $HEXPASS
